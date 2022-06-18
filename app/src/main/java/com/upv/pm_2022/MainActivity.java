53d0f32272c9e721e400b36014a9b480671cc9fa
uriegas <1930526@upv.edu.mx>
2022-06-18 16:21:28 -0500

    First Commit

---
+++ app/src/main/java/com/upv/pm_2022/MainActivity.java
@@ -0,0 +1,17 @@
+package com.upv.pm_2022;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
