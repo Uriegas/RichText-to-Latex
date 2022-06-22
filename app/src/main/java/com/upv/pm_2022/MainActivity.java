f79cbd054fb0dee202a4e7147abc6fe778000f65
uriegas <1930526@upv.edu.mx>
2022-06-22 12:19:06 -0500

    Features to support

--- app/src/main/java/com/upv/pm_2022/MainActivity.java
+++ app/src/main/java/com/upv/pm_2022/MainActivity.java
@@ -21,6 +21,8 @@ import jp.wasabeef.richeditor.RichEditor;
  * Based on 2020 Wasabeef
  * <p>
  * Modified by Eduardo Uriegas in June 2022 to support Latex parsing
+ * <p>
+ * Commented code is functionality not yet implemented in the HTML2LATEX parser
  */
 public class MainActivity extends AppCompatActivity {
 
@@ -47,9 +49,9 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.action_redo).setOnClickListener(view->{mEditor.redo();});
         findViewById(R.id.action_bold).setOnClickListener(view->{mEditor.setBold();});
         findViewById(R.id.action_italic).setOnClickListener(view->{mEditor.setItalic();});
-        findViewById(R.id.action_subscript).setOnClickListener(view->{mEditor.setSubscript();});
-        findViewById(R.id.action_superscript).setOnClickListener(view->{mEditor.setSuperscript();});
-        findViewById(R.id.action_strikethrough).setOnClickListener(view->{mEditor.setStrikeThrough();});
+//        findViewById(R.id.action_subscript).setOnClickListener(view->{mEditor.setSubscript();});
+//        findViewById(R.id.action_superscript).setOnClickListener(view->{mEditor.setSuperscript();});
+        findViewById(R.id.action_strikethr).setOnClickListener(view->{mEditor.setStrikeThrough();});
         findViewById(R.id.action_underline).setOnClickListener(view->{mEditor.setUnderline();});
         findViewById(R.id.action_heading1).setOnClickListener(view->{mEditor.setHeading(1);});
         findViewById(R.id.action_heading2).setOnClickListener(view->{mEditor.setHeading(2);});
@@ -65,41 +67,35 @@ public class MainActivity extends AppCompatActivity {
                 isChanged = !isChanged;
             }
         });
-        findViewById(R.id.action_bg_color).setOnClickListener(new View.OnClickListener() {
-            private boolean isChanged;
-            @Override
-            public void onClick(View v) {
-                mEditor.setTextBackgroundColor(isChanged ? Color.TRANSPARENT : Color.YELLOW);
-                isChanged = !isChanged;
-            }
-        });
-        findViewById(R.id.action_indent).setOnClickListener(view->{mEditor.setIndent();});
-        findViewById(R.id.action_outdent).setOnClickListener(view->{mEditor.setOutdent();});
-        findViewById(R.id.action_align_left).setOnClickListener(view->{mEditor.setAlignLeft();});
-        findViewById(R.id.action_align_center).setOnClickListener(view->{mEditor.setAlignCenter();});
-        findViewById(R.id.action_align_right).setOnClickListener(view->{mEditor.setAlignRight();});
-        findViewById(R.id.action_blockquote).setOnClickListener(view->{mEditor.setBlockquote();});
+//        findViewById(R.id.action_bg_color).setOnClickListener(new View.OnClickListener() {
+//            BUG: Transparent background not working, displays black Pixel 2 API 3
+//            private boolean isChanged;
+//            @Override
+//            public void onClick(View v) {
+//                mEditor.setTextBackgroundColor(isChanged ? Color.TRANSPARENT : Color.YELLOW);
+//                isChanged = !isChanged;
+//            }
+//        });
+//        findViewById(R.id.action_indent).setOnClickListener(view->{mEditor.setIndent();});
+//        findViewById(R.id.action_outdent).setOnClickListener(view->{mEditor.setOutdent();});
+//        findViewById(R.id.action_align_left).setOnClickListener(view->{mEditor.setAlignLeft();});
+//        findViewById(R.id.action_align_center).setOnClickListener(view->{mEditor.setAlignCenter();});
+//        findViewById(R.id.action_align_right).setOnClickListener(view->{mEditor.setAlignRight();});
         findViewById(R.id.action_insert_bullets).setOnClickListener(view->{mEditor.setBullets();});
         findViewById(R.id.action_insert_numbers).setOnClickListener(view->{mEditor.setNumbers();});
+        // TODO: Cannot dequote
+        findViewById(R.id.action_blockquote).setOnClickListener(view->{mEditor.setBlockquote();});
         findViewById(R.id.action_insert_image).setOnClickListener(view-> {
+            // TODO: Alert intent to select image, add default image
+            //       Ask if this should be supported in V1 since it is tricky to generate .tex with
+            //       this feature.
                 mEditor.insertImage("https://raw.githubusercontent.com/wasabeef/art/master/" +
                                         "chip.jpg", "dachshund", 320);
         });
-        findViewById(R.id.action_insert_youtube).setOnClickListener( view -> {
-                mEditor.insertYoutubeVideo("https://www.youtube.com/embed/u9CScOFfCM4");
-        });
-        findViewById(R.id.action_insert_audio).setOnClickListener(view -> {
-                mEditor.insertAudio("https://file-examples-com.github.io/uploads/2017/11/" +
-                                        "file_example_MP3_5MG.mp3");
-        });
-        findViewById(R.id.action_insert_video).setOnClickListener(view -> {
-                mEditor.insertVideo("https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/" +
-                                    "1080/Big_Buck_Bunny_1080_10s_10MB.mp4", 360);
-        });
         findViewById(R.id.action_insert_link).setOnClickListener(view -> {
+            // TODO: Alert intent to select link, add default link
             mEditor.insertLink("https://github.com/", "Github");
         });
-        findViewById(R.id.action_insert_checkbox).setOnClickListener(view->{mEditor.insertTodo();});
 
         exportBtn = findViewById(R.id.export);
         exportBtn.setOnClickListener(view -> {
@@ -110,6 +106,7 @@ public class MainActivity extends AppCompatActivity {
                 file.createNewFile();
                 FileWriter out = new FileWriter(Environment.getExternalStorageDirectory()
                         .toString() + '/' + OUTPUT);
+                // TODO: Add parser
                 out.append(mPreview.getText().toString()); out.flush(); out.close();
                 Toast.makeText(getBaseContext(), "DB exported into root folder",
                         Toast.LENGTH_LONG).show();
